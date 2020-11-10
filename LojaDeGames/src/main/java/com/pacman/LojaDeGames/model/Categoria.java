064effebbe72a7d4cdc89c75c69e6b9ee11f7c10
PauloNegri <paulo_negrii@hotmail.com>
2020-11-10 19:42:56 -0300

    porject 2.0, new table implemented

--- LojaDeGames/src/main/java/com/pacman/LojaDeGames/model/Categoria.java
+++ LojaDeGames/src/main/java/com/pacman/LojaDeGames/model/Categoria.java
@@ -1,16 +1,21 @@
 package com.pacman.LojaDeGames.model;
 
 import java.util.Date;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "categoria")
@@ -30,6 +35,12 @@ public class Categoria {
 	
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date date = new java.sql.Date(System.currentTimeMillis());
+	
+	@OneToMany(mappedBy = "categoria", cascade = CascadeType.ALL)
+	@JsonIgnoreProperties("categoria")
+	private List<Produto> produto;
+
+
 
 	public long getId() {
 		return id;
@@ -62,4 +73,13 @@ public class Categoria {
 	public void setDate(Date date) {
 		this.date = date;
 	} 
+	
+	public List<Produto> getProduto() {
+		return produto;
+	}
+	
+	public void setProduto(List<Produto> produto) {
+		this.produto = produto;
+	}
+
 }
---
+++ LojaDeGames/src/main/java/com/pacman/LojaDeGames/model/Produto.java
@@ -0,0 +1,64 @@
+package com.pacman.LojaDeGames.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "produto")
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	@Size(min = 3, max = 25)
+	private String name;
+	
+	private double valor;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getValor() {
+		return valor;
+	}
+
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	} 
+	
+}
